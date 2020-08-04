35e51eaa84eb3b579989be427edcd1cdd47faa6b
zb <zhubin@dra100.com>
2020-08-04 17:59:46 +0800

    修改接口规范

--- src/main/java/com/book/controller/BookHandler.java
+++ src/main/java/com/book/controller/BookHandler.java
@@ -2,6 +2,7 @@ package com.book.controller;
 
 import com.book.entity.Book;
 import com.book.repository.BookRepository;
+import com.book.result.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,20 +11,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/book")
 public class BookHandler {
     @Autowired
     private BookRepository bookRepository;
 
-//    @GetMapping("/findAll/{page}/{size}")
-//    public Page<Book> findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size){
-//        PageRequest request = PageRequest.of(page,size);
+    @GetMapping("/findAll")
+//    public Page<Book> findAll(Integer pages, Integer pageSize) {
+//        PageRequest request = PageRequest.of(pages,pageSize);
 //        return bookRepository.findAll(request);
 //    }
-    @GetMapping("/findAll")
-    public Object findAll(Integer pages, Integer pageSize) {
+    public JsonResult<List> findAll(Integer pages, Integer pageSize) {
+//        List<Book> userList = new ArrayList<>();
         PageRequest request = PageRequest.of(pages,pageSize);
-        return bookRepository.findAll(request);
+        List<Book> userList = new ArrayList<>(request.);
+        return new JsonResult<>(userList, "获取用户列表成功");
     }
 }
